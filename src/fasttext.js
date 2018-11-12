e49eacd21a42c0b5744262a9c3805ea3eb0508a0
Rafael Kallis <rk@rafaelkallis.com>
2018-11-12 17:56:11 +0100

    updated model, added vscode dataset pull

--- src/fasttext.js
+++ src/fasttext.js
@@ -14,7 +14,7 @@ const Table = require('cli-table');
 const ConfusionMatrix = require('ml-confusion-matrix');
 const franc = require('franc');
 
-const datasetPath = path.resolve(__dirname, '../dataset.csv');
+const datasetPath = path.resolve(__dirname, '../github_dataset.txt');
 const testPath = path.resolve(__dirname, '../test.txt');
 const trainPath = path.resolve(__dirname, '../train.txt');
 
@@ -33,92 +33,47 @@ exports.predict = predict;
  * Trains the model with all available data
  */
 async function train() {
-  fs.writeFileSync(trainPath, '',);
-  const reader = new Reader(datasetPath);
-  let row;
-  while (row = reader.next()) {
-    const [label, text] = row;
-    const line = `__label__${label} ${text}\n`;
-    fs.appendFileSync(trainPath, line);
-  }
   await classifier.train('supervised', {
-    input: trainPath,
+    input: datasetPath,
     output: path.resolve(__dirname, '../model'),
-    minCount: 25,
+    minCount: 14,
   });
 }
 exports.train = train;
 
-/**
- * Benchmarks the trained model against the test dataset
- */
-async function test() {
-  const liner = new Liner(testPath);
-  let line;
-  const actualLabels = [];
-  const predictedLabels = [];
-  while (line = liner.next()) {
-    try {
-      line = line.toString();
-      let [actual] = line.match(/^__label__[a-z]+/);
-      actual = actual.substring(9);
-      const text = line.substring(actual.length + 10);
-      const [prediction, similarity] = await predict(text);
-      actualLabels.push(actual);
-      predictedLabels.push(prediction);
-    } catch (e) {
-    }
-  }
-  const cm = ConfusionMatrix.fromLabels(actualLabels, predictedLabels);
-  const results = ['bug','enhancement', 'question'].reduce((o, label) => ({...o, [label]: {
-    precision: parseFloat(cm.getPositivePredictiveValue(label).toFixed(3)),
-    recall: parseFloat(cm.getTruePositiveRate(label).toFixed(3)),
-    f1: parseFloat(cm.getF1Score(label).toFixed(3)),
-  }}), {
-    accuracy: parseFloat(cm.getAccuracy().toFixed(3)),
-  });
-  return results;
-}
-exports.test = test;
-
 async function benchmark(folds = 10) {
   console.log(magenta(`starting ${folds} fold validation...`));
   const measures = [];
+  let liner, line;
   for (let i = 0; i < folds; i++) {
     fs.writeFileSync(testPath, '',);
     fs.writeFileSync(trainPath, '',);
-    const reader = new Reader(datasetPath);
-    let row;
-    while (row = reader.next()) {
-      const [label, text] = row;
-      const line = `__label__${label} ${text}\n`;
+    liner = new Liner(datasetPath);
+    while (line = liner.next()) {
+      line = line.toString();
       const bucket = parseInt(hash(line).substring(0, 11), 16) % folds;
-      fs.appendFileSync(bucket === i ? testPath : trainPath, line);
+      fs.appendFileSync(bucket === i ? testPath : trainPath, line + '\n');
     }
 
     await classifier.train('supervised', {
       input: trainPath,
       output: path.resolve(__dirname, '../model'),
-      minCount: 25,
+      minCount: 14,
     });
 
-    const liner = new Liner(testPath);
-    let line;
+    liner = new Liner(testPath);
     const actualLabels = [];
     const predictedLabels = [];
     while (line = liner.next()) {
-      try {
-        line = line.toString();
-        let [actual] = line.match(/^__label__[a-z]+/);
-        actual = actual.substring(9);
-        const text = line.substring(actual.length + 10);
+      line = line.toString();
+      let [actual] = line.match(/^__label__[a-z]+/);
+      actual = actual.substring(9);
+      const text = line.substring(actual.length + 10);
 
-        const [prediction, similarity] = await predict(text);
-        
-        actualLabels.push(actual);
-        predictedLabels.push(prediction);
-      } catch (e) {
-      }
+      const [prediction, similarity] = await predict(text);
+
+      actualLabels.push(actual);
+      predictedLabels.push(prediction);
     }
     const cm = ConfusionMatrix.fromLabels(actualLabels, predictedLabels);
     measures.push(['bug','enhancement', 'question'].reduce((o, label) => ({...o, [label]: {
@@ -128,7 +83,7 @@ async function benchmark(folds = 10) {
     }}), {
       accuracy: parseFloat(cm.getAccuracy().toFixed(3)),
     }));
-    console.log(magenta(`run ${i} finished`));
+    console.log(magenta(`run ${i+1}/${folds} finished`));
   }
   console.log(bgMagenta('  stats  '));
   console.log(magenta('accuracy: '), mean(measures.map(m => m.accuracy)).toFixed(3));
