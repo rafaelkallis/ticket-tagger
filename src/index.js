46b09722476517d34cd387f5d38a0abd8a319454
Rafael Kallis <rk@rafaelkallis.com>
2021-03-27 22:38:43 +0100

    fix: appinsights instrumentation key access

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const config = require("./config");
 
 if (config.APPINSIGHTS_INSTRUMENTATIONKEY) {
   appInsights
-    .setup(config.APP_INSIGHTS_INSTRUMENTATION_KEY)
+    .setup(config.APP_INSIGHTS_INSTRUMENTATIONKEY)
     .setSendLiveMetrics(true)
     .start();
 }
