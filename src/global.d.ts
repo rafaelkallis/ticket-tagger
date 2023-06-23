1c0fe4bbe3360808ec4985732055badc5a6693cb
Rafael Kallis <rk@rafaelkallis.com>
2023-06-23 17:01:06 +0000

    chore: updated fasttext lib

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,3 @@
-declare module "@rafaelkallis/fasttext" {
-	export class FastText {
-		static from(path: string): Promise<FastText>;
-		predict(text: string): Promise<[string, number][]>;
-	}
-}
-
 declare module "nunjucks-octicons-extension" {
 	import { Extension } from "nunjucks";
 	const nunjucksOcticonsExtension: Extension;
